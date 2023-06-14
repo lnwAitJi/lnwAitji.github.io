1c9e867553c0fbac5b67103b308f2243a7c265a2
Inw AitJi <jariya232560@gmail.com>
2023-06-14 20:39:51 +0700

    Dark Mode + Better

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = [
+        { id: "japanproject", url: "https://github.com/pooooooon/JavaPlayer" },
+        { id: "bluemyth", url: "https://github.com/BlueMyth-Network" },
+        { id: "cmgmmo", url: "../Minecraft/index.html" },
+        { id: "cmgmmodis", url: "https://discord.com/invite/JjchmEtqXp" },
+        { id: "project", url: "./ProJect/index.html" },
+        { id: "connect", url: "./CMGMMO/index.html" },
+        { id: "back", url: "https://lnwAitJi.github.io" }
+    ];
+
+    buttons.forEach(button => {
+        const element = document.getElementById(button.id);
+        if (element) {
+            element.addEventListener("click", () => {
+                if(button.url.startsWith("https://lnwAitJi.github.io")){
+                    window.location.href = button.url;
+                    return
+                }
+                if (button.url.startsWith("http")) {
+                    window.open(button.url, "_blank");
+                } else {
+                    window.location.href = button.url;
+                }
+            });
+        } else {
+            console.error(`Element with ID '${button.id}' not found.`);
+        }
+    });
+});
