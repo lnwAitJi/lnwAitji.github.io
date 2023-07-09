ded6ab64a66e515d33c15278e290f51315a67184
Inw AitJi <jariya232560@gmail.com>
2023-07-09 16:30:53 +0700

    Fix Bug

--- givegen/script.js
+++ givegen/script.js
@@ -41,28 +41,29 @@ function generateCommand() {
     let itemType = document.getElementById("itemType").value;
     let playerName = document.getElementById("playerName").value;
     let command = "give " + playerName + " " + itemType + " 1 0 ";
-    let canPlace = "";
-    let canDestroy = "";
+    let canPlace = false
+    let canDes = false
+    let canPlaceItem = ""
+    let canDesItem = ""
     if (canPlaceOn.length > 0) {
-        canPlace = '"minecraft:can_place_on":{"blocks":[';
+        canPlace = true
         for (let i = 0; i < canPlaceOn.length; i++) {
-            canPlace += '"' + canPlaceOn[i] + '",';
+            canPlaceItem += `"${canPlaceOn[i]}",`;
         }
-        canPlace = canPlace.slice(0, -1) + ']},';
+        canPlaceItem = canPlaceItem.substring(0, canPlaceItem.length - 1);
     }
-
     if (canDestroy.length > 0) {
-        canDestroy = '"minecraft:can_destroy":{"blocks":[';
+        canDes = true
         for (let i = 0; i < canDestroy.length; i++) {
-            canDestroy += '"' + canDestroy[i] + '",';
+            canDesItem += `"${canDestroy[i]}",`;
         }
-        canDestroy = canDestroy.slice(0, -1) + ']},';
+        canDesItem = canDesItem.substring(0, canDesItem.length - 1);
     }
-
-    let commandOutput = command;
-    if (canPlace !== "" || canDestroy !== "") {
-        commandOutput += '{' + canPlace + canDestroy.slice(0, -1) + '}';
-    }
-
-    document.getElementById("commandOutput").value = commandOutput;
+    let base = ""
+    if (canPlace === true && canDes === false) base = `{"minecraft:can_place_on":{blocks":[${canPlaceItem}]}}`
+    else if (canPlace === false && canDes === true) base = `{"minecraft:can_destroy":{blocks":[${canDesItem}]}}`
+    else if (canPlace === true && canDes === true) base = `{"minecraft:can_place_on":{"blocks":[${canPlaceItem}]},"minecraft:can_destroy":{"blocks":[${canDesItem}]}}`
+    else base = ""
+    command += base
+    document.getElementById("commandOutput").value = command;
 }
