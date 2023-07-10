44666fb46a4c99425eae6594e051e3bb5bd716e8
InwAitJi <100911929+lnwAitJi@users.noreply.github.com>
2023-07-10 15:27:47 +0700

    Fix bug 🐛 onRemoved
    Check on gencommand that del?

--- givegen/script.js
+++ givegen/script.js
@@ -1,49 +1,3 @@
-let canPlaceOn = [];
-let canDestroy = [];
-
-document.getElementById("itemType").addEventListener("input", generateCommand);
-document.getElementById("count").addEventListener("input", generateCommand);
-document.getElementById("id").addEventListener("input", generateCommand);
-document.getElementById("playerName").addEventListener("input", generateCommand);
-
-function addCanPlace() {
-    let blockType = document.getElementById("canPlace").value;
-    if (blockType !== "") {
-        canPlaceOn.push(blockType);
-        document.getElementById("canPlaceContainer").insertAdjacentHTML("beforeend", "<input type='text' value='" + blockType + "' disabled><button type='button' onclick='removeCanPlace(this)'>-</button><br>");
-        document.getElementById("canPlace").value = "";
-        generateCommand();
-    }
-}
-
-function addCanDestroy() {
-    let blockType = document.getElementById("canDestroy").value;
-    if (blockType !== "") {
-        canDestroy.push(blockType);
-        document.getElementById("canDestroyContainer").insertAdjacentHTML("beforeend", "<input type='text' value='" + blockType + "' disabled><button type='button' onclick='removeCanDestroy(this)'>-</button><br>");
-        document.getElementById("canDestroy").value = "";
-        generateCommand();
-    }
-}
-
-function removeCanPlace(button) {
-    let index = Array.prototype.indexOf.call(button.parentNode.children, button) - 2;
-    canPlaceOn.splice(index, 1);
-    button.parentNode.removeChild(button.previousSibling.previousSibling);
-    button.parentNode.removeChild(button.previousSibling);
-    button.parentNode.removeChild(button);
-    generateCommand();
-}
-
-function removeCanDestroy(button) {
-    let index = Array.prototype.indexOf.call(button.parentNode.children, button) - 2;
-    canDestroy.splice(index, 1);
-    button.parentNode.removeChild(button.previousSibling.previousSibling);
-    button.parentNode.removeChild(button.previousSibling);
-    button.parentNode.removeChild(button);
-    generateCommand();
-}
-
 function generateCommand() {
     let itemType = document.getElementById("itemType").value;
     let playerName = document.getElementById("playerName").value;
@@ -55,6 +9,7 @@ function generateCommand() {
     let canDes = false;
     let canPlaceItem = "";
     let canDesItem = "";
+
     if (canPlaceOn.length > 0) {
         canPlace = true;
         for (let i = 0; i < canPlaceOn.length; i++) {
@@ -62,6 +17,7 @@ function generateCommand() {
         }
         canPlaceItem = canPlaceItem.substring(0, canPlaceItem.length - 1);
     }
+
     if (canDestroy.length > 0) {
         canDes = true;
         for (let i = 0; i < canDestroy.length; i++) {
@@ -69,11 +25,29 @@ function generateCommand() {
         }
         canDesItem = canDesItem.substring(0, canDesItem.length - 1);
     }
+
     let base = "";
-    if (canPlace === true && canDes === false) base = `{"minecraft:can_place_on":{"blocks":[${canPlaceItem}]}}`;
-    else if (canPlace === false && canDes === true) base = `{"minecraft:can_destroy":{"blocks":[${canDesItem}]}}`;
-    else if (canPlace === true && canDes === true) base = `{"minecraft:can_place_on":{"blocks":[${canPlaceItem}]},"minecraft:can_destroy":{"blocks":[${canDesItem}]}}`;
-    else base = "";
+
+    if (canPlace === true && canDes === false) {
+        base = `{"minecraft:can_place_on":{"blocks":[${canPlaceItem}]}}`;
+    } else if (canPlace === false && canDes === true) {
+        base = `{"minecraft:can_destroy":{"blocks":[${canDesItem}]}}`;
+    } else if (canPlace === true && canDes === true) {
+        base = `{"minecraft:can_place_on":{"blocks":[${canPlaceItem}]},"minecraft:can_destroy":{"blocks":[${canDesItem}]}}`;
+    } else {
+        base = "";
+    }
+
     command += base;
+    
+    let deletedItems = canPlaceOn.concat(canDestroy);
+
+    for (let i = 0; i < deletedItems.length; i++) {
+        command = command.replace(`"${deletedItems[i]}",`, "");
+        command = command.replace(`"${deletedItems[i]}"`, "");
+    }
+    command = command.replace(/,}/g, "}");
+    command = command.replace(/,]/g, "]");
+
     document.getElementById("commandOutput").value = command;
 }
