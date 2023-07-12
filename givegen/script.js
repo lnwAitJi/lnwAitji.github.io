40b2a0527a0197edea6f9efe5e8cd59ced0efb9a
InwAitJi <100911929+lnwAitJi@users.noreply.github.com>
2023-07-12 08:02:50 +0700

    Update script.js

--- givegen/script.js
+++ givegen/script.js
@@ -1,99 +1,80 @@
 let canPlaceOn = [];
 let canDestroy = [];
 
-document.getElementById("itemType").addEventListener("input", generateCommand);
-document.getElementById("count").addEventListener("input", generateCommand);
-document.getElementById("id").addEventListener("input", generateCommand);
-document.getElementById("playerName").addEventListener("input", generateCommand);
-
 function addCanPlace() {
-    let blockType = document.getElementById("canPlace").value;
-    if (blockType !== "") {
-        canPlaceOn.push(blockType);
-        document.getElementById("canPlaceContainer").insertAdjacentHTML("beforeend", "<input type='text' value='" + blockType + "' disabled><button type='button' onclick='removeCanPlace(this)'>-</button><br>");
-        document.getElementById("canPlace").value = "";
-        generateCommand();
-    }
+  let canPlaceInput = document.getElementById("canPlace").value;
+  canPlaceOn.push(canPlaceInput);
 }
 
 function addCanDestroy() {
-    let blockType = document.getElementById("canDestroy").value;
-    if (blockType !== "") {
-        canDestroy.push(blockType);
-        document.getElementById("canDestroyContainer").insertAdjacentHTML("beforeend", "<input type='text' value='" + blockType + "' disabled><button type='button' onclick='removeCanDestroy(this)'>-</button><br>");
-        document.getElementById("canDestroy").value = "";
-        generateCommand();
-    }
+  let canDestroyInput = document.getElementById("canDestroy").value;
+  canDestroy.push(canDestroyInput);
 }
 
-function removeCanPlace(button) {
-    let index = Array.prototype.indexOf.call(button.parentNode.children, button) - 2;
-    canPlaceOn.splice(index, 1);
-    button.parentNode.removeChild(button.previousSibling.previousSibling);
-    button.parentNode.removeChild(button.previousSibling);
-    button.parentNode.removeChild(button);
-    generateCommand();
+function removeCanPlace(index) {
+  canPlaceOn.splice(index, 1);
 }
 
-function removeCanDestroy(button) {
-    let index = Array.prototype.indexOf.call(button.parentNode.children, button) - 2;
-    canDestroy.splice(index, 1);
-    button.parentNode.removeChild(button.previousSibling.previousSibling);
-    button.parentNode.removeChild(button.previousSibling);
-    button.parentNode.removeChild(button);
-    generateCommand();
+function removeCanDestroy(index) {
+  canDestroy.splice(index, 1);
 }
 
 function generateCommand() {
-    let itemType = document.getElementById("itemType").value;
-    let playerName = document.getElementById("playerName").value;
-    let id = document.getElementById("id").value;
-    let count = document.getElementById("count").value;
-    
-    let command = "give " + playerName + " " + itemType + " " + count + " " + id + " ";
-    let canPlace = false;
-    let canDes = false;
-    let canPlaceItem = "";
-    let canDesItem = "";
+  let itemType = document.getElementById("itemType").value;
+  let playerName = document.getElementById("playerName").value;
+  let id = document.getElementById("id").value;
+  let count = document.getElementById("count").value;
 
-    if (canPlaceOn.length > 0) {
-        canPlace = true;
-        for (let i = 0; i < canPlaceOn.length; i++) {
-            canPlaceItem += `"${canPlaceOn[i]}",`;
-        }
-        canPlaceItem = canPlaceItem.substring(0, canPlaceItem.length - 1);
+  let command = "give " + playerName + " " + itemType + " " + count + " " + id + " ";
+  let canPlace = false;
+  let canDes = false;
+  let canPlaceItem = "";
+  let canDesItem = "";
+
+  if (canPlaceOn.length > 0) {
+    canPlace = true;
+    for (let i = 0; i < canPlaceOn.length; i++) {
+      canPlaceItem += `"${canPlaceOn[i]}",`;
     }
+    canPlaceItem = canPlaceItem.substring(0, canPlaceItem.length - 1);
+  }
 
-    if (canDestroy.length > 0) {
-        canDes = true;
-        for (let i = 0; i < canDestroy.length; i++) {
-            canDesItem += `"${canDestroy[i]}",`;
-        }
-        canDesItem = canDesItem.substring(0, canDesItem.length - 1);
+  if (canDestroy.length > 0) {
+    canDes = true;
+    for (let i = 0; i < canDestroy.length; i++) {
+      canDesItem += `"${canDestroy[i]}",`;
     }
+    canDesItem = canDesItem.substring(0, canDesItem.length - 1);
+  }
 
-    let base = "";
+  let base = "";
 
-    if (canPlace === true && canDes === false) {
-        base = `{"minecraft:can_place_on":{"blocks":[${canPlaceItem}]}}`;
-    } else if (canPlace === false && canDes === true) {
-        base = `{"minecraft:can_destroy":{"blocks":[${canDesItem}]}}`;
-    } else if (canPlace === true && canDes === true) {
-        base = `{"minecraft:can_place_on":{"blocks":[${canPlaceItem}]},"minecraft:can_destroy":{"blocks":[${canDesItem}]}}`;
-    } else {
-        base = "";
-    }
+  if (canPlace === true && canDes === false) {
+    base = `{"minecraft:can_place_on":{"blocks":[${canPlaceItem}]}}`;
+  } else if (canPlace === false && canDes === true) {
+    base = `{"minecraft:can_destroy":{"blocks":[${canDesItem}]}}`;
+  } else if (canPlace === true && canDes === true) {
+    base = `{"minecraft:can_place_on":{"blocks":[${canPlaceItem}]},"minecraft:can_destroy":{"blocks":[${canDesItem}]}}`;
+  } else {
+    base = "";
+  }
 
-    command += base;
-    
-    let deletedItems = canPlaceOn.concat(canDestroy);
+  command += base;
 
-    for (let i = 0; i < deletedItems.length; i++) {
-        command = command.replace(`"${deletedItems[i]}",`, "");
-        command = command.replace(`"${deletedItems[i]}"`, "");
-    }
-    command = command.replace(/,}/g, "}");
-    command = command.replace(/,]/g, "]");
+  let deletedItems = canPlaceOn.concat(canDestroy);
 
-    document.getElementById("commandOutput").value = command;
+  for (let i = 0; i < deletedItems.length; i++) {
+    command = command.replace(`"${deletedItems[i]}",`, "");
+    command = command.replace(`"${deletedItems[i]}"`, "");
+  }
+  command = command.replace(/,}/g, "}");
+  command = command.replace(/,]/g, "]");
+
+  document.getElementById("commandOutput").value = command;
+}
+
+function copyCommandOutput() {
+  let commandOutput = document.getElementById("commandOutput");
+  commandOutput.select();
+  document.execCommand("copy");
 }
