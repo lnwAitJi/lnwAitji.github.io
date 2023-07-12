7a76c7f925a6f54eba938a3b27fcea5b27cade9a
InwAitJi <100911929+lnwAitJi@users.noreply.github.com>
2023-07-12 07:57:12 +0700

    Update script.js

--- givegen/script.js
+++ givegen/script.js
@@ -1,3 +1,49 @@
+let canPlaceOn = [];
+let canDestroy = [];
+
+document.getElementById("itemType").addEventListener("input", generateCommand);
+document.getElementById("count").addEventListener("input", generateCommand);
+document.getElementById("id").addEventListener("input", generateCommand);
+document.getElementById("playerName").addEventListener("input", generateCommand);
+
+function addCanPlace() {
+    let blockType = document.getElementById("canPlace").value;
+    if (blockType !== "") {
+        canPlaceOn.push(blockType);
+        document.getElementById("canPlaceContainer").insertAdjacentHTML("beforeend", "<input type='text' value='" + blockType + "' disabled><button type='button' onclick='removeCanPlace(this)'>-</button><br>");
+        document.getElementById("canPlace").value = "";
+        generateCommand();
+    }
+}
+
+function addCanDestroy() {
+    let blockType = document.getElementById("canDestroy").value;
+    if (blockType !== "") {
+        canDestroy.push(blockType);
+        document.getElementById("canDestroyContainer").insertAdjacentHTML("beforeend", "<input type='text' value='" + blockType + "' disabled><button type='button' onclick='removeCanDestroy(this)'>-</button><br>");
+        document.getElementById("canDestroy").value = "";
+        generateCommand();
+    }
+}
+
+function removeCanPlace(button) {
+    let index = Array.prototype.indexOf.call(button.parentNode.children, button) - 2;
+    canPlaceOn.splice(index, 1);
+    button.parentNode.removeChild(button.previousSibling.previousSibling);
+    button.parentNode.removeChild(button.previousSibling);
+    button.parentNode.removeChild(button);
+    generateCommand();
+}
+
+function removeCanDestroy(button) {
+    let index = Array.prototype.indexOf.call(button.parentNode.children, button) - 2;
+    canDestroy.splice(index, 1);
+    button.parentNode.removeChild(button.previousSibling.previousSibling);
+    button.parentNode.removeChild(button.previousSibling);
+    button.parentNode.removeChild(button);
+    generateCommand();
+}
+
 function generateCommand() {
     let itemType = document.getElementById("itemType").value;
     let playerName = document.getElementById("playerName").value;
