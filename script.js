f1764bae7264c3930c36d2ddff7540f681972ccc
InwAitJi <100911929+lnwAitJi@users.noreply.github.com>
2023-07-13 14:18:08 +0700

    using endswith ?ontab=true
    for thing on tab use ?ontab=true now!!

--- script.js
+++ script.js
@@ -5,27 +5,22 @@ document.addEventListener("DOMContentLoaded", () => {
         { id: "cmgmmo", url: "../minecraft/" },
         { id: "cmgmmodis", url: "https://discord.com/invite/JjchmEtqXp" },
         { id: "project", url: "./ProJect/" },
-        { id: "connect", url: "https://lnwaitji.github.io/Minecraft/CMGMMO" },
-        { id: "back", url: "https://lnwAitJi.github.io" }
+        { id: "connect", url: "https://lnwaitji.github.io/Minecraft/CMGMMO?ontab=true" },
+        { id: "back", url: "https://lnwAitJi.github.io?ontab=true" },
+        { id: "r", url: "https://lnwAitJi.github.io/database?ontab=true" }
     ];
 
     buttons.forEach(button => {
         const element = document.getElementById(button.id);
         if (element) {
             element.addEventListener("click", () => {
-                if(button.url.startsWith("https://lnwAitJi.github.io")){
+                if(button.url.endsWith("?ontab=true")){
                     window.location.href = button.url;
                     return
                 }
-if(button.url.startsWith("https://lnwaitji.github.io/Minecraft/CMGMMO")){
-                    window.location.href = button.url;
-                    return
-                }
-                if (button.url.startsWith("http")) {
-                    window.open(button.url, "_blank");
-                } else {
-                    window.location.href = button.url;
-                }
+                if (button.url.startsWith("http")) window.open(button.url, "_blank");
+                else window.location.href = button.url;
+                
             });
         } else {
             console.error(`Element with ID '${button.id}' not found.`);
